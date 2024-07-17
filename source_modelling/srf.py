09b4c10c6716cc28acd4f779292f4d9a6088ae93
Jake Faulkner <jakefaulkn@gmail.com>
2024-07-17 12:55:18 +1200

    add srf module

---
+++ source_modelling/srf.py
@@ -0,0 +1,380 @@
+"""
+Module for handling SRF (Standard Rupture Format) files.
+
+This module provides classes and functions for reading and writing SRF files,
+as well as representing their contents.
+See https://wiki.canterbury.ac.nz/display/QuakeCore/File+Formats+Used+On+GM
+for details on the SRF format.
+
+
+Classes:
+- SrfFile: Representation of an SRF file.
+
+Exceptions:
+- SrfParseError: Exception raised for errors in parsing SRF files.
+
+Functions:
+- read_srf: Read an SRF file into memory.
+- write_srf: Write an SRF object to a filepath.
+"""
+
+import dataclasses
+import re
+from pathlib import Path
+from typing import Optional, TextIO
+
+import numpy as np
+import pandas as pd
+
+PLANE_COUNT_RE = r"PLANE (\d+)"
+POINT_COUNT_RE = r"POINTS (\d+)"
+
+
+@dataclasses.dataclass
+class SrfFile:
+    """
+    Representation of an SRF file.
+
+    Attributes
+    ----------
+    version : str
+        The version of this SrfFile
+    header : pd.DataFrame
+        A list of SrfSegment objects representing the header of the SRF file.
+    points : pd.DataFrame
+        A list of SrfPoint objects representing the points in the SRF file.
+    """
+
+    version: str
+    header: pd.DataFrame
+    points: pd.DataFrame
+
+
+class SrfParseError(Exception):
+    """Exception raised for errors in parsing SRF files."""
+
+    pass
+
+
+def read_version(srf_file: TextIO):
+    """Read the version value from an srf file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file object to read from.
+    """
+    return float(srf_file.readline())
+
+
+def write_version(srf_file: TextIO):
+    """Write version value to an srf file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file object to write to.
+    """
+    srf_file.write("1.0\n")
+
+
+def read_srf_headers(srf_file: TextIO) -> pd.DataFrame:
+    """Read the header section of an SRF file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file object to read the headers from.
+
+    Raises
+    ------
+    SrfParseError
+        An SrfParseError is raised if an error occurs parsing the SRF file headers.
+
+    Returns
+    -------
+    list[SrfSegment]
+        A list of all the planes listed in the header of the SRF file.
+    """
+    plane_count_line = srf_file.readline().strip()
+    plane_count_match = re.match(PLANE_COUNT_RE, plane_count_line)
+    if not plane_count_match:
+        raise SrfParseError(f'Expecting PLANE header line, got: "{plane_count_line}"')
+    plane_count = int(plane_count_match.group(1))
+    segments = []
+
+    for _ in range(plane_count):
+        segments.append(
+            {
+                "elon": read_float(srf_file),
+                "elat": read_float(srf_file),
+                "nstk": read_int(srf_file),
+                "ndip": read_int(srf_file),
+                "len": read_float(srf_file),
+                "wid": read_float(srf_file),
+                "stk": read_float(srf_file),
+                "dip": read_float(srf_file),
+                "dtop": read_float(srf_file),
+                "shyp": read_float(srf_file),
+                "dhyp": read_float(srf_file),
+            }
+        )
+    return pd.DataFrame(segments)
+
+
+def read_float(srf_file: TextIO, label: Optional[str] = None) -> float:
+    """Read a float from an SRF file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file to read from.
+    label : Optional[str]
+        A human friendly label for the floating point (for debugging
+        purposes), or None for no label. Defaults to None.
+
+    Raises
+    ------
+    SrfParseError
+        If there is an error reading the float value from the SRF file.
+
+    Returns
+    -------
+    float
+        The float read from the SRF file.
+    """
+    while (cur := srf_file.read(1)).isspace():
+        pass
+    float_str = cur
+    while not (cur := srf_file.read(1)).isspace():
+        float_str += cur
+    try:
+        return float(float_str)
+    except ValueError:
+        if label:
+            raise SrfParseError(f'Expecting float ({label}), got: "{float_str}"')
+        else:
+            raise SrfParseError(f'Expecting float, got: "{float_str}"')
+
+
+def read_int(srf_file: TextIO, label: Optional[str] = None) -> int:
+    """Read a int from an SRF file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file to read from.
+
+    Raises
+    ------
+    SrfParseError
+        If there is an error reading the int value from the SRF file.
+
+    Returns
+    -------
+    int
+        The int read from the SRF file.
+    """
+    while (cur := srf_file.read(1)).isspace():
+        pass
+    int_str = cur
+    while not (cur := srf_file.read(1)).isspace():
+        int_str += cur
+    try:
+        return int(int_str)
+    except ValueError:
+        if label:
+            raise SrfParseError(f'Expecting int ({label}), got: "{int_str}"')
+        else:
+            raise SrfParseError(f'Expecting int, got: "{int_str}"')
+
+
+def read_points_count(srf_file: TextIO) -> int:
+    """Read the number of points contained in an srf file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file object to read from.
+
+    Raises
+    ------
+    SrfParseError
+        If the current line does not match the expected syntax for the number
+        of points in an SRF file (see POINT_COUNT_RE).
+
+    Returns
+    -------
+    int
+        The number of points contained in the SRF file.
+    """
+    points_count_line = srf_file.readline().strip()
+    points_count_match = re.match(POINT_COUNT_RE, points_count_line)
+    if not points_count_match:
+        raise SrfParseError(f'Expecting POINTS header line, got: "{points_count_line}"')
+    return int(points_count_match.group(1))
+
+
+def read_srf_point(srf_file: TextIO) -> dict[str, int | float]:
+    """Read a single SRF point from a file handle.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The file handle of the SRF file, pointing to the start of the
+        line.
+
+    Returns
+    -------
+    dict[str, int | float]
+        A dictionary containing the values for the point.
+    """
+    # skip optional first spaces
+    row = {
+        "lon": read_float(srf_file, label="lon"),
+        "lat": read_float(srf_file, label="lat"),
+        "dep": read_float(srf_file, label="dep"),
+        "stk": read_float(srf_file, label="stk"),
+        "dip": read_float(srf_file, label="dip"),
+        "area": read_float(srf_file, label="area"),
+        "tinit": read_float(srf_file, label="tinit"),
+        "dt": read_float(srf_file, label="dt"),
+        "rake": read_float(srf_file, label="rake"),
+        "slip1": read_float(srf_file, label="slip1"),
+    }
+    nt1 = read_int(srf_file, label="nt1")
+    row["slip2"] = read_float(srf_file, label="slip2")
+    nt2 = read_int(srf_file, label="nt2")
+    row["slip3"] = read_float(srf_file, label="slip3")
+    nt3 = read_int(srf_file, label="nt3")
+    row["slipt1"] = np.fromiter(
+        (read_float(srf_file, label="slipt1") for _ in range(nt1)), float
+    )
+    row["slipt2"] = np.fromiter(
+        (read_float(srf_file, label="slipt2") for _ in range(nt2)), float
+    )
+    row["slipt3"] = np.fromiter(
+        (read_float(srf_file, label="slipt2") for _ in range(nt3)), float
+    )
+    return row
+
+
+def read_srf(srf_ffp: Path) -> SrfFile:
+    """Read an SRF file into an SrfFile object.
+
+    Parameters
+    ----------
+    srf_ffp : Path
+        The filepath of the SRF file.
+
+    Returns
+    -------
+    SrfFile
+        The filepath of the SRF file.
+    """
+    with open(srf_ffp, mode="r", encoding="utf-8") as srf_file_handle:
+        version = read_version(srf_file_handle)
+        headers = read_srf_headers(srf_file_handle)
+        point_count = read_points_count(srf_file_handle)
+        points = pd.DataFrame(
+            (read_srf_point(srf_file_handle) for _ in range(point_count))
+        )
+        return SrfFile(version, headers, points)
+
+
+def write_srf_header(srf_file: TextIO, header: pd.DataFrame) -> None:
+    """Write a list of SRF plane segments as a header.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file to write to.
+    header : pd.DataFrame
+        The list of SrfSegments to write.
+    """
+    srf_file.write(f"PLANE {len(header)}\n")
+    srf_file.write(
+        header.to_string(
+            index=False,
+            header=None,
+            formatters={
+                "elon": lambda elon: f"{elon:.6f}",
+                "elat": lambda elat: f"{elat:.6f}",
+                "len": lambda len: f"{len:.4f}",
+                "wid": lambda wid: f"{wid:.4f}",
+            },
+        )
+        + "\n"
+    )
+
+
+def write_point_count(srf_file: TextIO, point_count: int) -> None:
+    """Write out a POINTS declaration for the number of points in an SRF file..
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file to write to.
+    point_count : int
+        The number of points to write.
+    """
+    srf_file.write(f"POINTS {point_count}\n")
+
+
+def write_slip(srf_file: TextIO, slips: np.ndarray) -> None:
+    """Write out slip values to an SRF file.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file to write to.
+    slips : np.ndarray
+        The slip values to write.
+    """
+    for i in range(0, len(slips), 6):
+        srf_file.write(
+            "  "
+            + "  ".join(f"{slips[j]:.5E}" for j in range(i, min(len(slips), i + 6)))
+            + "\n"
+        )
+
+
+def write_srf_point(srf_file: TextIO, point: pd.Series) -> None:
+    """Write out a single SRF point.
+
+    Parameters
+    ----------
+    srf_file : TextIO
+        The SRF file to write to.
+    point : pd.Series
+        The point to write.
+    """
+    srf_file.write(
+        f"{point['lon']:.6f} {point['lat']:.6f} {point['dep']:g} {point['stk']:g} {point['dip']:g} {point['area']:.4E} {point['tinit']:.4f} {point['dt']:.6E}\n"
+    )
+    srf_file.write(
+        f"{point['rake']:g} {point['slip1']:.4f} {len(point['slipt1'])} {point['slip2']:.4f} {len(point['slipt2'])} {point['slip3']:.4f} {len(point['slipt3'])}\n"
+    )
+    if len(point["slipt1"]):
+        write_slip(srf_file, point["slipt1"])
+    if len(point["slipt2"]):
+        write_slip(srf_file, point["slipt2"])
+    if len(point["slipt3"]):
+        write_slip(srf_file, point["slipt3"])
+
+
+def write_srf(srf_ffp: Path, srf: SrfFile) -> None:
+    """Write an SRF object to a filepath.
+
+    Parameters
+    ----------
+    srf_ffp : Path
+        The filepath to write the srf object to.
+    srf : SrfFile
+        The SRF object.
+    """
+    with open(srf_ffp, mode="w", encoding="utf-8") as srf_file_handle:
+        write_version(srf_file_handle)
+        write_srf_header(srf_file_handle, srf.header)
+        write_point_count(srf_file_handle, len(srf.points))
+        srf.points.apply(lambda point: write_srf_point(srf_file_handle, point), axis=1)
