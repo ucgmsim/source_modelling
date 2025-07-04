3fa395d7d8b6d76f05d9d263c8abae0301df5e8e
Jake Faulkner <jakefaulkn@gmail.com>
2025-07-04 04:37:42 +0000

    feat(srf): perform a faster geometric reallocation method for reading SRF values

--- source_modelling/srf_reader.pyx
+++ source_modelling/srf_reader.pyx
@@ -20,12 +20,12 @@ DTYPE = pnp.double
 # NOTE: sparse matrix structs do not support Python docstrings.
 cdef struct sparse_matrix:
     # An intermediate CSR matrix representation used for reading slip values.
-    # 
+    #
     # The CSR matrix representation is a highly compressed sparse matrix format
     # optimised for row operations and matrix multiplication. See the wikipedia
     # description of the CSR matrix format[0] for a description of how the col_ptr
     # and row_ptr arrays record the indices of the data.
-    # 
+    #
     # Attributes
     # -----------
     # rows : int
@@ -40,21 +40,23 @@ cdef struct sparse_matrix:
     #     Column index value array.
     # data : double*
     #     Entry value array.
-    # 
+    # len : size_t
+    #     The actual size of the array in memory.
     # References
     # ----------
     # [0]: https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_row_(CSR,_CRS_or_Yale_format)
-    
+
     int rows
     int row_counter
     ITYPE_t* row_ptr
     int entries
     ITYPE_t* col_ptr
     DTYPE_t* data
-     
+    size_t len
+
 
 cdef void malloc_sparse_matrix(sparse_matrix* slip_matrix, int num_rows):
-    """ Initialise an empty sparse matrix. 
+    """ Initialise an empty sparse matrix.
 
     Parameters
     ----------
@@ -71,8 +73,8 @@ cdef void malloc_sparse_matrix(sparse_matrix* slip_matrix, int num_rows):
     slip_matrix.data = NULL
 
 cdef void free_sparse_matrix(sparse_matrix slip_matrix):
-    """ Free a sparse matrix. 
-    
+    """ Free a sparse matrix.
+
     Parameters
     ----------
     slip_matrix : sparse_matrix
@@ -83,7 +85,7 @@ cdef void free_sparse_matrix(sparse_matrix slip_matrix):
     free(slip_matrix.data);
 
 cdef void read_slipt_values(FILE* srf_file, int start_column, int slip_count, sparse_matrix* slip_matrix):
-    """ Read slip values from srf_file into the sparse matrix as a new row. 
+    """ Read slip values from srf_file into the sparse matrix as a new row.
 
     Parameters
     ----------
@@ -102,30 +104,30 @@ cdef void read_slipt_values(FILE* srf_file, int start_column, int slip_count, sp
     # record it's starting position at the end of the current data array.
     slip_matrix.row_ptr[slip_matrix.row_counter] = slip_matrix.entries
     slip_matrix.row_counter += 1
-
     if slip_count > 0:
         slip_matrix.entries += slip_count
         # Either allocate or grow the slip matrix row, column and data arrays
         # to fit the new slip values.
         if slip_matrix.col_ptr == NULL:
-            slip_matrix.col_ptr = <ITYPE_t*> malloc(slip_matrix.entries * sizeof(ITYPE_t));
-        else:
-            slip_matrix.col_ptr = <ITYPE_t*>realloc(slip_matrix.col_ptr, slip_matrix.entries  * sizeof(ITYPE_t));
-
-        if slip_matrix.data == NULL:
-            slip_matrix.data = <DTYPE_t*>malloc(slip_matrix.entries * sizeof(DTYPE_t));
-        else:
-            slip_matrix.data = <DTYPE_t*>realloc(slip_matrix.data , slip_matrix.entries * sizeof(DTYPE_t));
-
+            slip_matrix.len = max(slip_matrix.entries, 64)
+            slip_matrix.col_ptr = <ITYPE_t*> malloc(slip_matrix.len * sizeof(ITYPE_t));
+            slip_matrix.data = <DTYPE_t*>malloc(slip_matrix.len * sizeof(DTYPE_t));
+        elif slip_matrix.entries > slip_matrix.len:
+            while slip_matrix.len < slip_matrix.entries:
+                  slip_matrix.len = slip_matrix.len + (slip_matrix.len >> 1) # slip_matrix length = 1.5 x old length
+            slip_matrix.data = <DTYPE_t*>realloc(slip_matrix.data , slip_matrix.len * sizeof(DTYPE_t));
+            slip_matrix.col_ptr = <ITYPE_t*>realloc(slip_matrix.col_ptr, slip_matrix.len  * sizeof(ITYPE_t));
+        if slip_matrix.col_ptr == NULL or slip_matrix.data == NULL:
+            raise MemoryError("Failed to allocate memory for column and data pointers.")
         for slip_index in range(slip_matrix.entries - slip_count, slip_matrix.entries):
             # The column ptr records the column for each entry in the data array.
             #           column index        = start column + current offset from start of this row's slip values
             #                                                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             slip_matrix.col_ptr[slip_index] = start_column + slip_index - slip_matrix.entries + slip_count
             fscanf(srf_file, "%lf", &slip_matrix.data[slip_index])
-    
+
 cdef void read_srf_points_loop(FILE* srf_file, int point_count, DTYPE_t[:, :] metadata, sparse_matrix* slipt1s, sparse_matrix* slipt2s, sparse_matrix* slipt3s):
-    """ Loop over the number of points and read in the SRF data from srf_file. 
+    """ Loop over the number of points and read in the SRF data from srf_file.
 
     Parameters
     ----------
@@ -180,13 +182,11 @@ cdef void read_srf_points_loop(FILE* srf_file, int point_count, DTYPE_t[:, :] me
 # NOTE: not sure what the return type should be here because it a C function
 # returning a Python value. So I have just left it blank.
 cdef sparse_matrix_to_csr(sparse_matrix matrix):
-    """ Convert the internal sparse matrix representation into a scipy sparse csr array. 
-
+    """ Convert the internal sparse matrix representation into a scipy sparse csr array.
     Parameters
     ----------
     matrix : sparse_matrix
         The matrix to convert.
-
     Returns
     -------
     csr_array
@@ -195,34 +195,15 @@ cdef sparse_matrix_to_csr(sparse_matrix matrix):
     if matrix.entries == 0:
         return None
 
-    
-    # Basically, we just have to copy the data from the arrays into numpy arrays.
-    # We do this in a loop to ensure that the values are owned by numpy
-    # (and hence free'd by python instead of leaking).
-    cdef np.ndarray[DTYPE_t, ndim=1] data = pnp.zeros(matrix.entries, dtype=DTYPE)
-    cdef np.ndarray[ITYPE_t, ndim=1] col_indices = pnp.zeros(matrix.entries, dtype=pnp.int64)
-    cdef np.ndarray[ITYPE_t, ndim=1] row_indices = pnp.zeros(matrix.rows + 1, dtype=pnp.int64)
-    cdef int i = 0
-    cdef int max_col_ind = 0  
+    # Copy the numpy arrays rather than write into them with a loop. Should hopefully mean that numpy frees this data itself.
+    cdef np.ndarray[DTYPE_t, ndim=1] data = pnp.asarray(<DTYPE_t[:matrix.entries]>matrix.data, copy=True)
+    cdef np.ndarray[ITYPE_t, ndim=1] col_indices = pnp.asarray(<ITYPE_t[:matrix.entries]>matrix.col_ptr, copy=True)
+    cdef np.ndarray[ITYPE_t, ndim=1] row_indices = pnp.asarray(<ITYPE_t[:matrix.rows+1]>matrix.row_ptr, copy=True)
 
-    for i in range(matrix.entries):
-        # These "=" assignments are done in Python, not C!
-        data[i] = matrix.data[i]
-        col_indices[i] = matrix.col_ptr[i]
-        if max_col_ind <  matrix.col_ptr[i]:
-            # This is a regular C assignment
-            max_col_ind = matrix.col_ptr[i]
-
-    i = 0
-    for i in range(matrix.rows):
-        # Also Python assignment.
-        row_indices[i] = matrix.row_ptr[i]
-    row_indices[matrix.rows] = matrix.entries
     return sp.sparse.csr_array((data, col_indices, row_indices))
-        
-        
+
 def read_srf_points(
-        srf_file_handle_py: TextIO, 
+        srf_file_handle_py: TextIO,
         point_count: int
     ) -> tuple[npt.NDArray[DTYPE], csr_array, csr_array, csr_array]:
     """ Read point_count SRF points from an SRF file.
@@ -250,7 +231,7 @@ def read_srf_points(
     """
     cdef np.ndarray[DTYPE_t, ndim=2] metadata = pnp.zeros([point_count, 13], dtype=DTYPE)
     cdef DTYPE_t[:, :] metadata_view = metadata
-    
+
     cdef sparse_matrix slip1ts
     cdef sparse_matrix slip2ts
     cdef sparse_matrix slip3ts
@@ -267,9 +248,9 @@ def read_srf_points(
     slip1ts_mat = sparse_matrix_to_csr(slip1ts)
     slip2ts_mat = sparse_matrix_to_csr(slip2ts)
     slip3ts_mat = sparse_matrix_to_csr(slip3ts)
-    
+
     free_sparse_matrix(slip1ts)
     free_sparse_matrix(slip2ts)
     free_sparse_matrix(slip3ts)
-    
+
     return metadata, slip1ts_mat, slip2ts_mat, slip3ts_mat
