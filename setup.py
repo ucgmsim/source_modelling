af78fc5fef67f9d4672560a16fa6a09cef53244c
Jake Faulkner <jakefaulkn@gmail.com>
2025-07-06 22:55:37 +0000

    fix(setup): ruff format setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
-from setuptools_rust import RustExtension, Binding
 import os
 
-
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
 
 setup(
     name="source_modelling",
     packages=["source_modelling"],
-    rust_extensions=[RustExtension("source_modelling.srf_parser", "source_modelling/srf_parser/Cargo.toml", binding=Binding.PyO3, native=True)],
+    rust_extensions=[
+        RustExtension(
+            "source_modelling.srf_parser",
+            "source_modelling/srf_parser/Cargo.toml",
+            binding=Binding.PyO3,
+            native=True,
+        )
+    ],
     include_package_data=True,
     use_scm_version=True,
     zip_safe=False,  # required for Rust extensions
