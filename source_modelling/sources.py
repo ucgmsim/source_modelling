correct depth calculation

source_modelling/sources.py
@@ -63,13 +63,14 @@ class Point:
     def top_m(self) -> float:
         """float: The top of the point source pseudo-geometry"""
         depth = self.bounds[-1]
-        return depth - np.sin(np.radians(self.dip)) / 2
+        return depth - self.width_m * np.sin(np.radians(self.dip)) / 2
 
     @property
     def bottom_m(self) -> float:
         """float: The bottom of the point source pseudo-geometry"""
-        depth = self.bounds[-1]
-        return depth + np.sin(np.radians(self.dip)) / 2
+        centroid_depth = self.bounds[-1]
+
+        return centroid_depth + self.width_m * np.sin(np.radians(self.dip)) / 2
 
     @classmethod
     def from_lat_lon_depth(cls, point_coordinates: np.ndarray, **kwargs) -> Self: