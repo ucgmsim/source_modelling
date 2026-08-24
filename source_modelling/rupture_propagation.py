2e9832734e69f1891f35bad7e8626f89361615b8
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-08-24 14:01:48 +1200

    Update source_modelling/rupture_propagation.py
    Co-authored-by: Andrew Ridden-Harper <52001209+AndrewRidden-Harper@users.noreply.github.com>

--- source_modelling/rupture_propagation.py
+++ source_modelling/rupture_propagation.py
@@ -512,7 +512,7 @@ def jump_points_from_rupture_tree(
     rupture_causality_tree : Tree
         A rupture causality tree.
     min_depth : float | None, optional
-        The minimum depth to consider jumping between, or ``None`` to allow jumps at all depth.
+        The minimum depth to consider jumping between, or ``None`` to allow jumps at all depths.
 
     Returns
     -------
